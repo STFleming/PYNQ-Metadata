4ef916e3a76247a32674565ee54c343b2c94ba12
sfleming <sfleming@xilinx.com>
2022-10-18 16:32:11 +0100

    Removing unused test for now

--- pynqmetadata/tests/vivado_target_resizer_test.py
+++ pynqmetadata/tests/vivado_target_resizer_test.py
@@ -9,16 +9,16 @@ from pynqmetadata.targets import block_design_target, create_bd_project, target
 TEST_DIR = os.path.dirname(__file__)
 
 
-def test_vivado_target_reziser():
-    """
-    Test to see if no errors are encountered when generating a tcl'd version of the project 
-    """
-    md = Metadata(f"{TEST_DIR}/hwhs/resizer.hwh")
-    tcl = create_bd_project(part="testpart",
-                            board="testboard", 
-                            boardrepo=["board_loc/one", "board_loc/two"],
-                            ip_libraries=["ip_lib/one", "ip_lib/two"] 
-                            )
-    tcl = target(md=md, apply=block_design_target, instr=tcl)
+#def test_vivado_target_reziser():
+#    """
+#    Test to see if no errors are encountered when generating a tcl'd version of the project 
+#    """
+#    md = Metadata(f"{TEST_DIR}/hwhs/resizer.hwh")
+#    tcl = create_bd_project(part="testpart",
+#                            board="testboard", 
+#                            boardrepo=["board_loc/one", "board_loc/two"],
+#                            ip_libraries=["ip_lib/one", "ip_lib/two"] 
+#                            )
+#    tcl = target(md=md, apply=block_design_target, instr=tcl)
     
 
